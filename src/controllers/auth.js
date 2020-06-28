71de8666877e60e6ee3fb3d4929a516a1255b742
Iury Vasconcelos <iury0393@gmail.com>
2020-06-28 19:13:10 -0300

    Subindo os middlewares e o controller

---
+++ src/controllers/auth.js
@@ -0,0 +1,56 @@
+const User = require("../models/User");
+const asyncHandler = require("../middlewares/asyncHandler");
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Checa se o email e o pw não estam vazios
+  if (!email || !password) {
+    return next({
+      message: "Por favor digite um email e uma senha",
+      statusCode: 400,
+    });
+  }
+
+  // Checa se o usuário existe
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return next({
+      message: "Esse email não está registrado com uma conta",
+      statusCode: 400,
+    });
+  }
+
+  // Se existe, checa se a senha bate com a salva
+  const match = await user.checkPassword(password);
+
+  if (!match) {
+    return next({ message: "A senha não está correta", statusCode: 400 });
+  }
+  const token = user.getJwtToken();
+
+  // Então manda o json como resposta
+  res.status(200).json({ success: true, token });
+});
+
+exports.signup = asyncHandler(async (req, res) => {
+  const { fullname, username, email, password } = req.body;
+
+  const user = await User.create({ fullname, username, email, password });
+
+  const token = user.getJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
+
+exports.me = asyncHandler(async (req, res) => {
+  const { avatar, username, fullname, email, _id, website, bio } = req.user;
+
+  res
+    .status(200)
+    .json({
+      success: true,
+      data: { avatar, username, fullname, email, _id, website, bio },
+    });
+});
---
+++ src/middleware/auth.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+exports.protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return next({
+      message: "Você precisa estar logado para visitar essa rota",
+      statusCode: 403,
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id).select("-password");
+
+    if (!user) {
+      return next({ message: `Nenhum usuário encontrado para o id: ${decoded.id}` });
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    next({
+      message: "Você precisa estar logado para visitar essa rota",
+      statusCode: 403,
+    });
+  }
+};
