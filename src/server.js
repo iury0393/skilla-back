234c5309bcf6d6ab49fadf625552bd68179d7d88
Iury Vasconcelos <iury0393@gmail.com>
2020-06-28 20:34:30 -0300

    Corrigindo o server

--- src/server.js
+++ src/server.js
@@ -22,5 +22,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 3333;
 app.listen(
   PORT,
-  console.log(`Server started: ${process.env.NODE_ENV}, port: ${PORT}`)
+  console.log(`Server started, port: ${PORT}`)
 );
