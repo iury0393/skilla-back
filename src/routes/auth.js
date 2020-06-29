2c1b60fc9d33d461818b63916ea6a7c87c3ee5ba
Iury Vasconcelos <iury0393@gmail.com>
2020-06-28 23:35:14 -0300

    Subindo rota para listar usuarios

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const router = express.Router();
-const { login, signup, me } = require("../controllers/auth");
+const { login, signup, me, users } = require("../controllers/auth");
 const { protect } = require("../middlewares/auth");
 
 router.route("/register").post(signup);
 router.route("/logon").post(login);
 router.route("/me").get(protect, me);
+router.route("/users").get(users);
 
 module.exports = router;
