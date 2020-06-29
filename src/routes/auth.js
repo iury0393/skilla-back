5fc4b038c3a6649255130b97525ee1c6b96f59a5
Iury Vasconcelos <iury0393@gmail.com>
2020-06-29 19:24:31 -0300

    Mudando a rota

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { login, signup, me, users } = require("../controllers/auth");
 const { protect } = require("../middlewares/auth");
 
-router.route("/register").post(signup);
+router.route("/signup").post(signup);
 router.route("/login").post(login);
 router.route("/me").get(protect, me);
 router.route("/users").get(users);
